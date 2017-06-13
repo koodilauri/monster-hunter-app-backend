1d95be3785e5f2bb7417165eda36eb4d8f3f62e7
Lauri <lakoivisto@gmail.com>
2017-06-13 12:40:41 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+var cors = require('cors')
+const app = express()
+
+var corsOptions = {
+  origin: 'https://monster-hunter-app.herokuapp.com/',
+  optionsSuccessStatus: 200
+}
+
+const port = 8081
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World")
+})
+
+app.get("/submissions", cors(corsOptions), (req, res, next) => {
+  res.json({
+    submissions: [
+      { "name": 'tobi',  "time":"05:11:00", "type":1},
+      { "name":"lauri", "time":"07:46:00","type":2}
+    ]})
+})
+
+app.listen(port, (err) => {
+  if (err) {
+     console.log(err)
+  } else {
+    console.log("App is listening on port " + port)
+  }
+})
