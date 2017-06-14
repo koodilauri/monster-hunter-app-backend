50f3e6654aea2f788885b2a8c595132829b67275
Lauri <lakoivisto@gmail.com>
2017-06-14 21:35:09 +0300

    edit config variables

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,21 @@
 const pg = require('pg');
+require('dotenv').config()
+
+const v = process.env.DB_USER;
 
 const config = {
-  user: 'mh_db_user',
-  password: '1qaz2wsx',
-  host: 'localhost',
-  port: process.env.DB_NAME,
-  database: 'mh_db',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  // port: process.env.DB_NAME,
+  database: process.env.DB_NAME,
   // connectionString: process.env.DATABASE_URL,
   max: 10, // max number of clients in the pool 
   idleTimeoutMillis: 30000, //   how long a client is allowed to remain idle before being closed 
 };
 
 process.on('unhandledRejection', e => {
-  console.log(">", process.env.DB_USER, "<")
+  console.log(config)
   console.log(e.message, e.stack)
 })
 
