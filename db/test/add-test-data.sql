d4deef3c7a26f82dd80ce2f2449c2647a35f1202
Lauri <lakoivisto@gmail.com>
2017-07-17 18:21:07 +0300

    edit db

--- db/test/add-test-data.sql
+++ db/test/add-test-data.sql
@@ -2,8 +2,8 @@ INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Harvest
 INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Harvest Tour: Dunes', 1, 'Guild', 'Survive until time expires or deliver a Paw Pass Ticket', 'None', 'MHGen');
 INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Harvest Tour: Verdant Hills', 1, 'Guild', 'Survive until time expires or deliver a Paw Pass Ticket', 'None', 'MHGen');
 INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Harvest Tour: Arctic Ridge', 1, 'Guild', 'Survive until time expires or deliver a Paw Pass Ticket', 'None', 'MHGen');
-INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Harvest Tour: Misty Peaks', 1, 'Guild', 'Survive until time expires or deliver a Paw Pass Ticket', 'None', 'MHGen');
-INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Boot to the Head', 1, 'Guild', 'Hunt a Great Maccao', 'Deliver 5 Abyssal Mushrooms', 'MHGen');
+INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Harvest Tour: Misty Peaks', +1, 'Guild', 'Survive until time expires or deliver a Paw Pass Ticket', 'None', 'MHGen');
+INSERT INTO quest (name, stars, questGiver, goal, subgoal, game) VALUES('Boot to the Head', +1, 'Guild', 'Hunt a Great Maccao', 'Deliver 5 Abyssal Mushrooms', 'MHGen');
 
 INSERT INTO submission (name, questId, questtime, weapon, style, created) VALUES ('lauri', 1, '00:05:31', 'Hammer', 'adept', '2017-06-12');
 INSERT INTO submission (name, questId, questtime, weapon, style, created) VALUES ('bob', 2, '00:06:02', 'Lance', 'striker', '2017-06-13');
