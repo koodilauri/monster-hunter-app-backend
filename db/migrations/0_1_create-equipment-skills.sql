9460a30f4fd8ee25b1b32668841c8189cb6a43d4
Lauri <lakoivisto@gmail.com>
2017-07-19 17:01:06 +0300

    edit db usage

--- db/migrations/0_1_create-equipment-skills.sql
+++ db/migrations/0_1_create-equipment-skills.sql
@@ -66,3 +66,4 @@ CREATE TABLE hunter_art (
 );
 
 GRANT ALL PRIVILEGES ON TABLE skill, armor, decoration, weapon, charm, hunter_art TO mh_db_user;
+GRANT USAGE, SELECT ON SEQUENCE charm_id_seq TO mh_db_user;
