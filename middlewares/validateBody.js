c14ea54225d9b541685fc91dfb9cd5bb002d3783
Lauri <lakoivisto@gmail.com>
2017-08-09 18:05:40 +0300

    edit armorset NULL

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -9,7 +9,6 @@ exports.validateBody = (name, schema) => (req, res, next) => {
   }
   const validation = _.get(validations, `${name}.${schema}`)
   const result = inspector.validate(validation, req.body)
-  console.log(result, validation)
   if (result.error.length !== 0) {
     throw new errors.BadRequestError(`${result.error[0].property} ${result.error[0].message}`, result.error)
   } else {
