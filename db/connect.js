5063a0eb9b141ce87dfaee3e16f86e18a794f117
Teemu Koivisto <tkoivisto456@gmail.com>
2017-08-21 15:36:07 +0300

    Add flyway

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,9 @@
 const pg = require('pg');
 
 const config = {
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   max: 10, // max number of clients in the pool 
-  idleTimeoutMillis: 30000, //   how long a client is allowed to remain idle before being closed 
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
 };
 
 process.on('unhandledRejection', e => {
