6aeea5417a1cb3770864a1b0d797f819e0d6a051
Lauri <lakoivisto@gmail.com>
2017-08-23 14:38:41 +0300

    edit test data

--- db/test/add-test-data.sh
+++ db/test/add-test-data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-psql mh_db < ./db/migrations/V1_0__create-equipment-skills.sql
-psql mh_db < ./db/migrations/V1_1__create-quest.sql
-psql mh_db < ./db/migrations/V1_3__create-armorset.sql
-psql mh_db < ./db/migrations/V1_4__create-submission.sql
+npm run db:migrate
 
 psql mh_db < ./db/test/skills.sql
 psql mh_db < ./db/test/add-test-data.sql
